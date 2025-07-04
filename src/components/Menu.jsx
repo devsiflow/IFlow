192a9b6615c246c2c2efd17c173069109a3e31fa
Klx07 <leandroklaen16@gmail.com>
2025-07-04 15:34:14 -0300

    Logo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,38 @@
-export default function Menu() {
+import logo from "../assets/logo.png";
+
+function Menu() {
   return (
-    <nav className="bg-green-900 text-white px-6 py-3 flex items-center justify-between">
-      {/* Logo */}
-      <div className="text-2xl font-bold tracking-wide">
-        <span className="text-yellow-400">IFlow</span>
+    <nav className="bg-green-950 text-white px-6 py-3 flex items-center justify-between">
+      {/* Logo e Links lado a lado */}
+      <div className="flex items-center space-x-8">
+        <img src={logo} alt="Logo" className="w-32" />
+        {/* Logo */}
+        <div className="text-2xl font-bold tracking-wide"></div>
+
+        {/* Links */}
+        <ul className="flex space-x-6">
+          <li>
+            <a href="#" className="hover:text-gray-300">
+              Como Funciona
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-300">
+              Objetivo
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-300">
+              Sobre nós
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-300">
+              Contato
+            </a>
+          </li>
+        </ul>
       </div>
-
-      {/* Links */}
-      <div className="bg-white">
-
-</div>
-
-      <ul className="flex space-x-6 text-left font-medium ">
-        <li><a href="#" className="hover:text-gray-300">Como Funciona</a></li>
-        <li><a href="#" className="hover:text-gray-300">Objetivo</a></li>
-        <li><a href="#" className="hover:text-gray-300">Sobre nós</a></li>
-        <li><a href="#" className="hover:text-gray-300">Contato</a></li>
-      </ul>
 
       {/* Botões */}
       <div className="flex items-center space-x-4">
@@ -28,3 +44,5 @@ export default function Menu() {
     </nav>
   );
 }
+
+export default Menu;

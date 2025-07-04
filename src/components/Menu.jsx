5b2fe3403e8e0f41d2647197abbe36269e5ad457
Klx07 <leandroklaen16@gmail.com>
2025-07-04 10:48:15 -0300

    Update Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,26 +1,30 @@
 export default function Menu() {
-    return (
-      <nav className="bg-green-900 text-white px-6 py-3 flex items-center justify-between">
-        {/* Logo */}
-        <div className="text-2xl font-bold tracking-wide">
-          <span className="text-yellow-400">IFlow</span>
-        </div>
-  
-        {/* Links */}
-        <ul className="flex space-x-6 font-medium">
-          <li><a href="#" className="hover:text-gray-300">Como Funciona</a></li>
-          <li><a href="#" className="hover:text-gray-300">Objetivo</a></li>
-          <li><a href="#" className="hover:text-gray-300">Sobre nós</a></li>
-          <li><a href="#" className="hover:text-gray-300">Contato</a></li>
-        </ul>
-  
-        {/* Botões */}
-        <div className="flex items-center space-x-4">
-          <button className="hover:text-gray-300">Entrar</button>
-          <button className="bg-green-500 hover:bg-green-400 text-white font-semibold px-4 py-1 rounded">
-            Cadastro
-          </button>
-        </div>
-      </nav>
-    );
-  }
+  return (
+    <nav className="bg-green-900 text-white px-6 py-3 flex items-center justify-between">
+      {/* Logo */}
+      <div className="text-2xl font-bold tracking-wide">
+        <span className="text-yellow-400">IFlow</span>
+      </div>
+
+      {/* Links */}
+      <div className="bg-white">
+
+</div>
+
+      <ul className="flex space-x-6 text-left font-medium ">
+        <li><a href="#" className="hover:text-gray-300">Como Funciona</a></li>
+        <li><a href="#" className="hover:text-gray-300">Objetivo</a></li>
+        <li><a href="#" className="hover:text-gray-300">Sobre nós</a></li>
+        <li><a href="#" className="hover:text-gray-300">Contato</a></li>
+      </ul>
+
+      {/* Botões */}
+      <div className="flex items-center space-x-4">
+        <button className="hover:text-gray-300">Entrar</button>
+        <button className="bg-green-500 hover:bg-green-400 text-white font-semibold px-4 py-1 rounded">
+          Cadastro
+        </button>
+      </div>
+    </nav>
+  );
+}
